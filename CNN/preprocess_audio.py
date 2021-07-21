a4da5613e69dec270592233f38d454f9954d17ea
edward-h-81 <edgh123.eh@gmail.com>
2021-07-21 15:32:15 +0100

    wav to log mel spectrogram py, wav examples

--- CNN/preprocess_audio.py
+++ CNN/preprocess_audio.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # bird sound file converted and displayed as a log mel spectrogram
-bird_sound = "ff1010bird_1051.wav"
+
+# bird_sound = "ff1010bird_1051.wav"
+# bird_sound = "BirdVox_bird.wav"
+bird_sound = "warblr_bird.wav"
 
 audio, sr = librosa.load(bird_sound, sr=22050)
 
@@ -13,10 +16,31 @@ log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
 librosa.display.specshow(log_mel_spectrogram,
                          x_axis="time",
                          y_axis="mel",
-                         sr=sr, cmap='gray') # cmap='gray' OR cmap='gray_r' - add for grayscale
+                         sr=sr, cmap='gray_r') # cmap='gray' OR cmap='gray_r' - add for grayscale
 plt.colorbar(format="%+2.f dB")
+plt.title("Bird sound present")
 plt.show()
 
 print(log_mel_spectrogram.shape)
 
+# wav file, without bird sound, converted and displayed as a log mel spectrogram
+
+# sound = "ff1010nobird_1945.wav"
+# sound = "BirdVox_nobird.wav"
+sound = "warblr_nobird.wav"
+
+audio, sr = librosa.load(sound, sr=22050)
+
+mel_spectrogram = librosa.feature.melspectrogram(audio, sr=22050, n_fft=2048, hop_length=512, n_mels=256)
+log_mel_spectrogram = librosa.power_to_db(mel_spectrogram, ref=np.max)
+
+librosa.display.specshow(log_mel_spectrogram,
+                         x_axis="time",
+                         y_axis="mel",
+                         sr=sr, cmap='gray_r') # cmap='gray' OR cmap='gray_r' - add for grayscale
+plt.colorbar(format="%+2.f dB")
+plt.title("Bird sound absent")
+plt.show()
+
+print(log_mel_spectrogram.shape)
 
---
+++ CNN/process_audio.py
@@ -0,0 +1,78 @@
+import json
+import os
+import librosa
+import math
+import matplotlib.pyplot as plt
+import librosa, librosa.display
+
+DATASET_PATH = "Sorted_114/warblr"
+SAMPLE_RATE = 22050
+DURATION = 10
+SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
+JSON_PATH = "warblr_1_segment.json"
+
+def save_log_mel_spectrogram(dataset_path, json_path, n_mels=256, n_fft=2048, hop_length=512, num_segments=5):
+
+    # dictionary to store data
+    data = {
+        "mapping": [],
+        "log mel spectrogram": [],
+        "labels": []
+    }
+
+    num_samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+    expected_num_log_mel_spectrogram_vectors_per_segment = math.ceil(num_samples_per_segment / hop_length) # ceil rounds up e.g., 1.2 -> 2
+
+    # loop through bird/nobird
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # ensure not at root level
+        if dirpath is not dataset_path:
+
+            # save the label
+            dirpath_components = dirpath.split("/")[-1]
+            semantic_label = dirpath_components
+            data["mapping"].append(semantic_label)
+            print("\nProcessing {}".format(semantic_label))
+
+            # process files for bird/nobird
+            for f in filenames:
+
+                # load the audio file
+                file_path = os.path.join(dirpath, f)
+                signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
+
+                # process segments extracting log mel spectrogram and storing data
+                for s in range(num_segments):
+                    start_sample = num_samples_per_segment * s
+                    finish_sample = start_sample + num_samples_per_segment
+
+                    mel_spectrogram = librosa.feature.melspectrogram(signal[start_sample:finish_sample],
+                                                                         sr=sr,
+                                                                         n_fft=n_fft,
+                                                                         n_mels=n_mels,
+                                                                         hop_length=hop_length)
+
+                    log_mel_spectrogram = librosa.power_to_db(mel_spectrogram)
+
+                    # librosa.display.specshow(log_mel_spectrogram,
+                    #                          x_axis="time",
+                    #                          y_axis="mel",
+                    #                          sr=sr, cmap='gray_r')  # cmap='gray' OR cmap='gray_r' - add for grayscale
+                    # plt.colorbar(format="%+2.f dB")
+                    # plt.title("Bird sound present")
+                    # plt.show()
+
+                    log_mel_spectrogram = log_mel_spectrogram.T
+
+                    # store log mel spectrogram for segment if it has the expected length
+                    if len(log_mel_spectrogram) == expected_num_log_mel_spectrogram_vectors_per_segment:
+                        data["log mel spectrogram"].append(log_mel_spectrogram.tolist())
+                        data["labels"].append(i-1) # -1 as the first iteration was for the dataset path
+                        print("{}, segment:{}".format(file_path, s+1))
+
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
+
+if __name__ == "__main__":
+    save_log_mel_spectrogram(DATASET_PATH, JSON_PATH, num_segments=1)
